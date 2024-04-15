7361f1928a6497d94508f72c246050d6a491c251
SrJohn369 <srjohn963@gmail.com>
2024-04-15 00:05:19 -0300

    v1.16.24.8 Alteração no arquivo urls.py

--- WorkStationSchool/urls.py
+++ WorkStationSchool/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('loginInicio.urls')),
@@ -10,4 +12,4 @@ urlpatterns = [
     path('disciplina/', include('disciplina.urls')),
     path('docente/', include('docente.urls')),
     path('turma/', include('turma.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
