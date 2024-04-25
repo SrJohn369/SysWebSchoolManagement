4d0d17ff0aa0c73a24b57cd8f7a9b45fda1577d0
SrJohn369 <srjohn963@gmail.com>
2024-04-25 09:08:23 -0300

    v1.16.24.4: Concertando endpoints

--- api/urls.py
+++ api/urls.py
@@ -4,7 +4,7 @@ from api.views import DirecaoAPI, DocentesAPI, AlunoAPI
 app_name = 'api'
 
 urlpatterns = [
-    path('diretores/', DocentesAPI.as_view(), name='docentes'),
-    path('docentes/', DirecaoAPI.as_view(), name='direções'),
+    path('docentes/', DocentesAPI.as_view(), name='docentes'),
+    path('diretores/', DirecaoAPI.as_view(), name='direções'),
     path('alunos/', AlunoAPI.as_view(), name='alunos'),
 ]
